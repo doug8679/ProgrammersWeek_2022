f3d8928858a28784ee7d9bb4f94bbcc4aa06262e
Doug Braxton <doug8679@users.noreply.github.com>
2022-08-24 10:29:29 -0400

    Incorporate baseline monolithic app in demo repo (#1)
    * Create the base react application for the monolithic rewrite
    
    I'm going to start with a single application React re-write of the www.micro-frontends.org example tractor store and
    progress from there into breaking the application into distinct MFE components.  Application was initialized using
    the npx create-mfe-app script.
    
    * Clean up webpack.config.js
    
    Removed the ModuleFederation plugin references from the webpack fiel as this version is intended
    to be a monolithic application.  The plugin will not be needed in this version.
    
    * Restructure repository layout
    
    * Implement basic tractor store from demo site
    
    More or less a direct copy over of the example code from https://github.com/neuland/micro-frontends to
    a react application, solely implemented in the App component.  No images working in this commit, page
    structure and style only.
    
    * Add public folder for images
    
    Fixes images not showing in monolithic app, partly.  Need to update paths to image files in the
    App component as well.
    
    * Update image paths in App
    
    Corrected image paths in App ocmponent for proper display in browser.
    Also began implementing click events and state management.  Changing the
    selected variant should update page state now.
    
    * Finish adding click handlers
    
    Completed state management implementation (minimally) with click handlers on relevant portions
    of the page.  NOTE: state keeps resetting, not a primary focus of this baseling refactor, but
    annoying all the same.

---
+++ tractor-store-react/src/App.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+// data
+const product = {
+  name: 'Tractor',
+  variants: [
+    {
+      sku: 't_porsche',
+      color: 'red',
+      name: 'Porsche-Diesel Master 419',
+      image: 'tractor-red.jpg',
+      thumb: 'tractor-red-thumb.jpg',
+      price: '66,00 €',
+    },
+    {
+      sku: 't_fendt',
+      color: 'green',
+      name: 'Fendt F20 Dieselroß',
+      image: 'tractor-green.jpg',
+      thumb: 'tractor-green-thumb.jpg',
+      price: '54,00 €',
+    },
+    {
+      sku: 't_eicher',
+      color: 'blue',
+      name: 'Eicher Diesel 215/16',
+      image: 'tractor-blue.jpg',
+      thumb: 'tractor-blue-thumb.jpg',
+      price: '58,00 €',
+    },
+  ],
+};
+
+const recos = {
+  t_porsche: ['3', '5', '6'],
+  t_fendt: ['3', '6', '4'],
+  t_eicher: ['1', '8', '7'],
+};
+const state = {
+  variant: 't_porsche',
+  basket: 0
+};
+
+const App = () => {
+
+  const [myState, setAppState] = useState(state);
+
+  const handleClickBuy = React.useCallback(() => {
+    setAppState({...myState, basket: myState.basket+1});
+  }, []);
+
+  const handleClickOption = React.useCallback((e) => {
+    const sku = e.currentTarget.getAttribute('data-sku');
+    setAppState({...myState, variant: sku});
+  }, []);
+  
+  const renderOption = (variant) => {
+    const active = myState.variant === variant.sku ? 'active' : '';
+    return (
+      <button key={variant.sku} className={active} type="button" data-sku={variant.sku} onClick={handleClickOption}>
+        <img src={variant.thumb} alt={variant.name} />
+      </button>
+    );
+  };
+
+  const renderReco = (id) => {
+    return <img key={id} src={`reco_${id}.jpg`} alt={`Reco ${id}`} />;
+  };
+
+  const variant = product.variants.find((v) => myState.variant === v.sku);
+  const reco = recos[variant.sku];
+  
+  return (
+    <>
+      <h1 id="store">The Model Store</h1>
+      <div id="basket" className={myState.basket === 0 ? 'empty' : ''}>basket: {myState.basket} item(s)</div>
+      <div id="image"><div><img src={variant.image} alt={variant.name} /></div></div>
+      <h2 id="name">{product.name} <small>{variant.name}</small></h2>
+      <div id="options">{product.variants.map(renderOption)}</div>
+      <button id="buy" type="button" onClick={handleClickBuy}>buy for {variant.price}</button>
+      <div id="reco"><h3>Related Products</h3>{reco.map(renderReco)}</div>
+    </>
+  )
+};
+ReactDOM.render(<App />, document.getElementById("app"));
